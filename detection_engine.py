c4a5fed61ad549168ad5ef6d0aed4c9b63593f87
salifjahangir <salif2k@gmail.com>
2026-04-30 22:54:13 +0530

    Behaviour detector class written and integrated

--- detection_engine.py
+++ detection_engine.py
@@ -34,10 +34,45 @@ def detect_suspicious(name, command):
     
     for word in command_words:
         if len(word) > 20 and _base64_char_check(word):
-            return (True, "Likely Base64 encoded command detected")
+            return (True, "Potential Base64 encoded command detected")
         
     
     return (False, "No suspicious process detected")
+
+
+class BehaviourDetector:
+    """Class for handling detection of suspicious burst commands."""
+    def __init__(self):
+        self.cmd_tracked = {}
+        self.cmd_alerted = set()
+        self.time_threshold = 5
+        self.max_events = 3
+
+    def process(self, command, start_time):
+        if command not in self.cmd_tracked:
+            self.cmd_tracked[command] = []
+        timestamps = self.cmd_tracked[command]
+        if len(timestamps) == self.max_events:
+            timestamps.pop(0)
+            timestamps.append(start_time)
+
+        else:
+            timestamps.append(start_time)
+
+        if len(timestamps) == self.max_events:
+            if (timestamps[-1] - timestamps[0] <= self.time_threshold):
+                if command not in self.cmd_alerted:
+                    self.cmd_alerted.add(command)
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+                
+
+        
 
 
 
--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 import psutil as ps
 import detection_engine as deng
+import time
 
-for process in ps.process_iter():
-    pid = None
-    try:
-        pid = process.pid
-        name = process.name()
-        cmd = process.cmdline()
-        if not cmd:
-            cmd_str = "N/A"
-        else:
-            cmd_str = " ".join(map(str, cmd))
+# Defining a set to hold the PID values already seen.
+pids_seen = set()
+#Defining the dictionary for holding command strings along with their list of timestamps.
+cmd_tracked = {}
+# Defining a set to hold the commands that have already been flagged for suspicious behaviour.
+cmd_alerted = set()
+# Initializing an object for tracking suspicious behaviour of commands.
+detect_behaviour = deng.BehaviourDetector()
+while True:
+    for process in ps.process_iter():
+        pid = None
 
-        is_suspicious, comment = deng.detect_suspicious(name, cmd_str)
-        if is_suspicious:
-            print(f"PID: {pid} | Name: {name} | CMD: {cmd_str}")
-            print(f"[!] {comment}")
-    except (ps.AccessDenied, ps.NoSuchProcess):
-        print(f"PID: {pid} | Access denied or process has ended")
+        try:
+            pid = process.pid
+            name = process.name()
+            cmd = process.cmdline()
+
+            if pid not in pids_seen:
+                pids_seen.add(pid)
+                start_time = time.time()
+            else:
+                continue
+            # Storing process command as a string.
+            if not cmd:
+                cmd_str = "N/A"
+            else:
+                cmd_str = " ".join(map(str, cmd))
+
+            is_suspicious, comment = deng.detect_suspicious(name, cmd_str)
+            if is_suspicious:
+                print(f"PID: {pid} | Name: {name} | CMD: {cmd_str}")
+                print(f"[!] {comment}")
+
+                # Detecting burst behaviour of suspicious commands.
+                if cmd_str == "N/A":
+                    continue
+                else:
+                    suspicious_behaviour = detect_behaviour.process(cmd_str, start_time)
+                    if suspicious_behaviour:
+                        print(f"Suspicious behaviour of command detected | Command: {cmd_str}")
+                        
+        except (ps.AccessDenied, ps.NoSuchProcess):
+            print(f"PID: {pid} | Access denied or process has ended")
 
